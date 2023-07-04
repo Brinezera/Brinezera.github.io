0a8f45c5e94f3a27fabc2d1baac6af90ba97729a
andrebrine11@gmail.com <andrebrine11@gmail.com>
2023-07-04 18:53:24 -0300

    Second Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const prevButton = document.querySelector(".carousel-prev");
+  const nextButton = document.querySelector(".carousel-next");
+  const carouselContainer = document.querySelector(".carousel-container");
+
+  prevButton.addEventListener("click", function() {
+    carouselContainer.scrollBy({
+      left: -carouselContainer.offsetWidth,
+      behavior: "smooth"
+    });
+  });
+
+  nextButton.addEventListener("click", function() {
+    carouselContainer.scrollBy({
+      left: carouselContainer.offsetWidth,
+      behavior: "smooth"
+    });
+  });
+});
